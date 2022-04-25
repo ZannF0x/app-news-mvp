796cfbef320982c3463be4550b8ad11c837789e1
Zannardy Andrade Oliveira <zannardy.andrade@mobapps.com.br>
2022-04-25 10:42:25 -0300

    improvement/initial commit and adding dependencies

---
+++ app/src/main/java/com/example/app_news/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.app_news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.app_news.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate:")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
